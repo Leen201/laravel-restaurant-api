1ab7b203bf7073ced6ac3b2b5c5dfb13169ae961
Leen201 <angelinakondratskaya@yandex.ru>
2022-11-06 16:03:33 +0600

    Added render for ModelNotFoundException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -41,10 +43,19 @@ class Handler extends ExceptionHandler
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
         $this->reportable(function (Throwable $e) {
             //
         });
     }
+
+    public function render($request, Throwable $e): Response|JsonResponse|\Symfony\Component\HttpFoundation\Response
+    {
+        if(get_class($e) == "Illuminate\Database\Eloquent\ModelNotFoundException") {
+            return (new Response('Model not found.', 404));
+        }
+        return parent::render($request, $e);
+    }
+
 }
